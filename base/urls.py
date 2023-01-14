8940730a166940b4861cccc14198a0dd7ed71562
Gefen hurvitz <gefenhurvitz@Gefens-MacBook-Air.local>
2023-01-14 20:49:17 +0200

    adding dummy data as python file to check the routes

--- base/urls.py
+++ base/urls.py
@@ -2,6 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('products', views.getProducts),
     
 ]
--- base/views.py
+++ base/views.py
@@ -1,4 +1,9 @@
 from django.http import JsonResponse
+from .products import products
  
 def index(req):
     return JsonResponse('hello', safe=False)
+
+
+def getProducts(req):
+    return JsonResponse(products, safe=False)
